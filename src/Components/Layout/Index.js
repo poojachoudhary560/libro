e04df557817d664b4cb41ba7d2848f056b385308
Pooja Choudhary <poojachoudhary560@gmail.com>
2019-06-04 15:45:04 +0530

    added context api

---
+++ src/Components/Layout/Index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Tracks from '../Tracks/Tracks';
+const Index = () => {
+  return(
+    <Fragment>
+      <Tracks />
+    </Fragment>
+  );
+}
+
+export default Index;
---
+++ src/Components/Layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <nav className="">
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo center">Libretto</a>
+
+      </div>
+    </nav>
+
+  );
+}
+
+export default Navbar;
